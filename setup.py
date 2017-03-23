682ecd3f7acdafc4e903664bba5b95b958128adc
Martin Olveyra <molveyra@gmail.com>
2017-03-23 19:53:30 -0300

    added info for pypi deployment

--- setup.py
+++ setup.py
@@ -5,8 +5,27 @@ from setuptools import setup, find_packages
 setup(
     name         = 'kafka_scanner',
     version      = '0.3.0',
+    description   = 'High Level Kafka Scanner, supporting inverse consuming and deduplication. Based on kafka-python library.',
+    keywords = 'kafka',
+    author = 'Scrapinghub',
+    author_email = 'info@scrapinghub',
+    license = 'BSD',
+    url = 'https://github.com/scrapinghub/kafka-scanner',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
     packages     = find_packages(),
     install_requires = [
         'kafka-python',
+        'six',
+        'retrying',
+        'msgpack-python',
+        'sqlitedict'
     ]
 )
